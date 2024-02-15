4dc352f2c09f5461c7f0043258ba87b488a35de0
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-02-15 20:48:56 +0300

    16 - useReducer

--- src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -1,42 +1,44 @@
-import React, {useState} from "react";
-
+import React, { useReducer } from "react";
+import { reducer } from "./reducer";
 
 type AccordionPropsType = {
-    titleValue: string
-    // collapsed: boolean
-}
+  titleValue: string;
+  // collapsed: boolean
+};
 
-
+// --------------------------------------------------------------------------------------------------------------
 export function UncontrolledAccordion(props: AccordionPropsType) {
+  //  const [collapsed, setCollapsed] = useState(false);
+  const [state, dispatch] = useReducer(reducer, { collapsed: true });
 
-    // const collapsed = false
-    const [collapsed, setCollapsed] = useState(false)
-
-    return <div>
-        <AccordionTitle title={props.titleValue} onClick={() => { setCollapsed(!collapsed)}}/>
-        {/*<button onClick={() => { setCollapsed(!collapsed)}}>Toggle</button>*/}
-        {!collapsed && <AccordionBody/>}
+  return (
+    <div>
+      {/*<AccordionTitle title={props.titleValue} onClick={() => {setCollapsed(!collapsed)}}/>;*/}
+      <AccordionTitle
+        title={props.titleValue}
+        onClick={() => {
+          dispatch({ type: "TOGGLE-COLLAPSED" });
+        }}
+      />
+      {!state.collapsed && <AccordionBody />}
     </div>
+  );
 }
-
 
 type AccordionPropsTitleProps = {
-    title: string
-    onClick:()=>void
-}
+  title: string;
+  onClick: () => void;
+};
 const AccordionTitle: React.FC<AccordionPropsTitleProps> = (props) => {
-    return (
-        <h3 onClick={()=>props.onClick()}>{props.title}</h3>
-    )
-}
-
+  return <h3 onClick={() => props.onClick()}>{props.title}</h3>;
+};
 
 function AccordionBody() {
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    )
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  );
 }
