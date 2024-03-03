67a410dd3ae620f1db8ce36d18e5d2fa05549db2
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-03-04 00:06:31 +0300

    useMemo

--- src/stories/UseMemoUseCallback.stories.tsx
+++ src/stories/UseMemoUseCallback.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 export default {
   title: "useMemo",
@@ -60,11 +60,6 @@ export const HelpsToReactMemo = () => {
   const [counter, setCounter] = useState(0);
   const [users, setUsers] = useState<string[]>(["Dimych", "Valera", "Artem"]);
 
-  // const addUser = () => {
-  //   const newUsers = [...users, "Sveta " + new Date().getTime()];
-  //   setUsers(newUsers);
-  // };
-
   const newArray = useMemo(() => {
     const newArray = users.filter((u) => u.toLowerCase().indexOf("a") > -1);
     return newArray;
@@ -91,48 +86,65 @@ export const HelpsToReactMemo = () => {
       >
         add user
       </button>
-      {/*<button onClick={addUser}>add user</button>*/}
-      {/*<NewMessagesCounter count={counter} />*/}
       {counter}
       <Users users={newArray} />
     </>
   );
 };
 
-// export const HelpsForReactMemoExample = () => {
-//   const [a, setA] = useState(5);
-//   const [b, setB] = useState(5);
-//
-//   let resultA = 1;
-//   let resultB = 1;
-//
-//   resultA = useMemo(() => {
-//     let tempResultA = 1;
-//     for (let i = 1; i <= a; i++) {
-//       let fake = 0;
-//       while (fake < 100000000) {
-//         fake++;
-//         const fakeValue = Math.random();
-//       }
-//       tempResultA = tempResultA * i;
-//     }
-//     return tempResultA;
-//   }, [a]);
-//
-//   for (let i = 1; i <= b; i++) {
-//     resultB = resultB * i;
-//   }
-//
-//   return (
-//     <>
-//       <input value={a} onChange={(event) => setA(+event.currentTarget.value)} />
-//       <input
-//         value={b}
-//         onChange={(event) => setB(Number(event.currentTarget.value))}
-//       />
-//       <hr />
-//       <div>Result for a:{resultA}</div>
-//       <div>Result for b:{resultB}</div>
-//     </>
-//   );
-// };
+export const LikesUseCallback = () => {
+  console.log("LikesUseCallback");
+  const [counter, setCounter] = useState(0);
+  const [books, setBooks] = useState<string[]>(["React", "JS", "CSS", "HTML"]);
+
+  // const addBook = () => {
+  //   const newUsers = [...books, "Angular " + new Date().getTime()];
+  //   setBooks(newUsers);
+  // };
+  const memoizedAddBook = useMemo(() => {
+    return () => {
+      const newUsers = [...books, "Angular " + new Date().getTime()];
+      setBooks(newUsers);
+    };
+  }, [books]);
+
+  const memoizedAddBook2 = useCallback(() => {
+    console.log(books);
+    const newUsers = [...books, "Angular " + new Date().getTime()];
+    setBooks(newUsers);
+  }, [books]);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        +
+      </button>
+      {counter}
+      <Book addBook={memoizedAddBook2} />
+    </>
+  );
+};
+
+type BookSecretPropsType = {
+  addBook: () => void;
+};
+
+const BooksSecret = (props: BookSecretPropsType) => {
+  console.log("BooksSecret");
+  return (
+    <div>
+      <button
+        onClick={() => {
+          props.addBook();
+        }}
+      >
+        add book
+      </button>
+    </div>
+  );
+};
+const Book = React.memo(BooksSecret);
