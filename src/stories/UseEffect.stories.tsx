f6f0c724f422bd7c20c0071a2c8581717b7fa89a
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-04-03 20:46:22 +0300

    хук useEffect

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -5,18 +5,71 @@ export default {
 };
 
 export const SimpleExample1 = () => {
+  const [fake, setFake] = useState(1);
   const [counter, setCounter] = useState(1);
   console.log("SimpleExample");
 
   useEffect(() => {
-    console.log("useEffect");
+    console.log("useEffect every render");
     document.title = counter.toString();
   });
+
+  useEffect(() => {
+    console.log("useEffect only first render(componentDidMount)");
+    document.title = counter.toString();
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect first render and every counter change");
+    document.title = counter.toString();
+  }, [counter]);
 
   return (
     <>
       Hello, {counter}
+      <button onClick={() => setFake(fake + 1)}>+</button>
       <button onClick={() => setCounter(counter + 1)}>+</button>
     </>
+  );
+};
+
+export const SimpleExample = () => {
+  const [fake, setFake] = useState(1);
+  const [counter, setCounter] = useState(1);
+  console.log("SimpleExample");
+
+  useEffect(() => {
+    setInterval(() => {
+      console.log("tick:" + counter);
+      setCounter(counter + 1);
+    }, 1000);
+  }, []);
+
+  return (
+    <>
+      Hello, counter: {counter} -fake: {fake}
+      {/*<button onClick={() => setFake(fake + 1)}>+</button>*/}
+      {/*<button onClick={() => setCounter(counter + 1)}>+</button>*/}
+    </>
+  );
+};
+
+export const Clock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2>{time.toLocaleTimeString()}</h2>
+    </div>
   );
 };
