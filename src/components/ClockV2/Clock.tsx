9d5f3be57bc80f0a4675213485d4a20ca6af525b
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-05-07 14:18:27 +0300

    Компонента часов обобщенная

---
+++ src/components/ClockV2/Clock.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { DigitalClockView } from "./DigitalClockView";
+import { AnalogClockView } from "./AnalogClockView";
+
+type PropsType = {
+  mode?: "analog" | "digital";
+};
+
+export const ClockV2: React.FC<PropsType> = (props) => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  let view;
+  switch (props.mode) {
+    case "analog":
+      view = <AnalogClockView date={date} />;
+      break;
+    case "digital":
+      view = <DigitalClockView date={date} />;
+  }
+
+  return <div>{view}</div>;
+};
+
+export type ClockAnalogView = {
+  date: Date;
+};
