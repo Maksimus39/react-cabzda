12a7452c1514cc0c2513693ae6695291d7ecebad
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-06-15 09:05:01 -0700

    ReactMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,40 @@
 import './App.css'
-import {Rating} from "./rating/Rating.tsx";
-import {Accordion} from "./accordion/Accordion.tsx";
-import {PageTitle} from "./appTitle/AppTitle.tsx";
-import {OnOff} from "./onOff/OnOff.tsx";
-import {UncontrolledAccordion} from "./uncontrolledAccordion/UncontrolledAccordion.tsx";
-import {UncontrolledRating} from "./uncontrolledRating/UncontrolledRating.tsx";
-import {useState} from "react";
+import ReactMemo from "./reactMemo/ReactMemo.tsx";
 
 function App() {
-    const [uncontrolAccordion, setUncontrolAccordion] = useState(false);
+    // const [uncontrolAccordion, setUncontrolAccordion] = useState(false);
 
     return (
         <div className="App">
-            <PageTitle title={"This is App Component"}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
-            <PageTitle title={"This is my Friends"}/>
+            {/*<PageTitle title={"This is App Component"}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+            {/*<PageTitle title={"This is my Friends"}/>*/}
 
 
-            <Accordion title={"Title-1"} collapsed={true}/>
-            <Accordion title={"Title-2"} collapsed={false}/>
+            {/*<Accordion title={"Title-1"} collapsed={true}/>*/}
+            {/*<Accordion title={"Title-2"} collapsed={false}/>*/}
 
 
-            <Rating/>
+            {/*<Rating/>*/}
 
 
-            <OnOff/>
-            <OnOff/>
-            <OnOff/>
-            <OnOff/>
+            {/*<OnOff/>*/}
+            {/*<OnOff/>*/}
+            {/*<OnOff/>*/}
+            {/*<OnOff/>*/}
 
 
-            <UncontrolledAccordion title={"Uncontrolled---------------"}
-                                   uncontrolAccordion={() => setUncontrolAccordion(!uncontrolAccordion)}/>
-            <UncontrolledAccordion title={"Uncontrolled-2"}/>
+            {/*<UncontrolledAccordion title={"Uncontrolled---------------"}*/}
+            {/*                       uncontrolAccordion={() => setUncontrolAccordion(!uncontrolAccordion)}/>*/}
+            {/*<UncontrolledAccordion title={"Uncontrolled-2"}/>*/}
 
-            <UncontrolledRating/>
+            {/*<UncontrolledRating/>*/}
+
+            <ReactMemo/>
         </div>
     )
 }
---
+++ src/reactMemo/ReactMemo.tsx
@@ -0,0 +1,37 @@
+import {useState} from "react";
+import * as React from "react";
+
+const Counter = (props: { count: number }) => {
+    console.log('Counter')
+    return <div>{props.count}</div>
+}
+
+type UsersArray = {
+    users: string[]
+}
+const Users = (props: UsersArray) => {
+    console.log('USERS')
+    return props.users.map((u, i) => <div key={i}>{u}</div>)
+}
+
+const UsersMemo = React.memo(Users)
+
+const ReactMemo = () => {
+   // console.log('REACT_MEMO')
+    const [count, setCount] = useState(0)
+    const [users, setUsers] = useState(['Max', 'Larisa', 'Andrey', 'Bogdan'])
+
+    const setCountHandler = () => {
+        setCount(count + 1)
+    }
+
+    return (
+        <>
+            <button onClick={setCountHandler}>+</button>
+            <UsersMemo users={users}/>
+            <Counter count={count}/>
+        </>
+    );
+};
+
+export default ReactMemo;
